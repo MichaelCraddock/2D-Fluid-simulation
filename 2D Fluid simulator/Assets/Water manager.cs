8b2cda56dec3097b53dcc530fc3699d4b7abacc5
Michael2 <Michael2>
2016-05-26 15:30:39 +0100

    Some initial variables.

---
+++ 2D Fluid simulator/Assets/Water manager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Watermanager : MonoBehaviour {
+    float[] xPos;
+    float[] yPos;
+    float[] veloc;
+    float[] accel;
+    LineRenderer Body;
+    GameObject[] meshobj;
+    Mesh[] meshes;
+    GameObject Colliders;
+    const float springconst = 0.02f;
+    const float damping = 0.04f;
+    const float spread = 0.05f;
+    const float z = -1f;
+    float baseheight;
+    float left;
+    float bottom;
+    public GameObject splash;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
